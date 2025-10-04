68dc2950567aca2330c27a16c734c5275c2641c3
peyman <peyman.alad@gmail.com>
2025-10-04 21:41:43 +0330

    Init

---
+++ src/main/java/org/example/billproject/BillProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.billproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/billproject/BillProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.billproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
